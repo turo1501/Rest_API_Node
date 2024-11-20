cc1c38b6ed83348d1c0fcd86a03ad7315f91a7d6
turo151001 <tuant32456@gmail.com>
2024-11-20 23:44:35 +0700

    first comment

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const Product = require 
+const app = express()
+
+app.use(express.json())
+
+app.post('/api/products' , async (req , res)=>{
+    try{
+       const product = await Product.create(req.body)
+       res.status(200).json(product)
+    }
+    catch(error){
+        res.status(599).json({message:error.message})
+    }
+})
+
+
+
+
+app.get('/', (req,res)=> res.send('hello 2'))
+mongoose.connect('mongodb+srv://nguyentrananhtuan151001:Tuan0908650255@cluster0.tcvij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=> {
+    console.log('Connected!')
+    app.listen(3000,()=>{ 
+        console.log('hello')
+    })
+} )
+.catch(()=>{
+    console.log('connect failed!')
+})
