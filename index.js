930e73d6923878370ffc9f5c4c7494f05853c325
John <tuant32456@gmail.com>
2024-11-23 00:09:31 +0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const Product = require 
+const Product = require('./models/product.model')
 const app = express()
 
 app.use(express.json())
@@ -15,7 +15,24 @@ app.post('/api/products' , async (req , res)=>{
     }
 })
 
-
+app.get('/api/products' , async(req,res)=>{
+    try{
+        const product = await Product.find({})
+        res.status(200).json(product)
+    }
+    catch(error){
+        res.status(599).json({message:error.message})
+    }
+})
+app.get('/api/products/:id' , async(req,res)=>{
+    try{
+        const product = await Product.find({})
+        res.status(200).json(product)
+    }
+    catch(error){
+        res.status(599).json({message:error.message})
+    }
+})
 
 
 app.get('/', (req,res)=> res.send('hello 2'))
